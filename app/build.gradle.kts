72a187cd28e9efc804847f256a5473375ddf7bd9
ts.Ryu <lts06069@naver.com>
2026-06-29 11:26:19 +0900

    자녀용 길 찾기 게임 - 비공개 테스트 진행 (1.0.2 / 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.rts.rys.ryy.wayfinding"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
